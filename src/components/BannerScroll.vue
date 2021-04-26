cb8421a9677701fdb39e8ba020f2cef27ef72a5c
Crystal-st <1980970807@qq.com>
2021-04-26 23:24:10 +0800

    引入confront图标并设置图标格式

---
+++ src/components/BannerScroll.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main">
+    <!-- 左侧菜单 -->
+    <div>
+      <!-- 主菜单 -->
+      <div class="menu">
+        
+      </div>
+      <!-- 子菜单 -->
+    </div>
+    <!-- 轮播图 -->
+    <div class="banner">
+      <!-- 共4张图片 -->
+      <a href="#">
+        <div class="picture_banner pic1"></div>
+      </a>
+      <a href="#">
+        <div class="picture_banner pic2"></div>
+      </a>
+      <a href="#">
+        <div class="picture_banner pic3"></div>
+      </a>
+      <a href="#">
+        <div class="picture_banner pic4"></div>
+      </a>
+    </div>
+    <!-- 轮播图上的左右按钮 -->
+    <div class="pic_button_left">
+      <i class="iconfont">&#xe665;</i>
+    </div>
+    <div class="pic_button_right">
+      <i class="iconfont">&#xe666;</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BannerScroll',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+
+</style>
